f774a815964084bf5af6ae2464f731e7dff31c95
AyratGizz <gizzatullin-airat@mail.ru>
2023-09-25 23:42:29 +0500

    Task3, Task4

---
+++ HW2/Task4.java
@@ -0,0 +1,22 @@
+package HW2;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        emptyString();
+    }
+
+    private static void emptyString() {
+        System.out.println("Введите текст: ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        if (string.equals("")) {
+            throw new RuntimeException("Пустые строки вводить нельзя!");
+
+        }
+        System.out.println("Вы ввели: " + string);
+        scanner.close();
+    }
+}
